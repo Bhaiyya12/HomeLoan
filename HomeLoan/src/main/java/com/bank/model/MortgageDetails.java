621b8f0f63fd5097a516cfed72b7f4279e0fa255
VasantiGaikwad <vasantisankpal95@gmail.com>
2023-12-11 12:55:50 +0530

    hello

--- HomeLoan/src/main/java/com/bank/model/MortgageDetails.java
+++ HomeLoan/src/main/java/com/bank/model/MortgageDetails.java
@@ -1,5 +1,18 @@
 package com.bank.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class MortgageDetails {
+	@Id
+	private int mortageId;
+	private double mortagePropertyValue;
+	private byte[] mortagePropertyProof;
+	private String mortagePropertyType;
+	private double mortageLoanOnProperty;
+	private byte[] mortagePropertyInsurance;
 
 }
