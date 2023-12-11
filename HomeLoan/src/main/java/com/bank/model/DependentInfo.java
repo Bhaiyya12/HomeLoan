b87a3e8f34317d0be13596e0ef1370c0ede16548
FDJ158 <janhvi.kshirsagar73@gmail.com>
2023-12-11 12:52:57 +0530

    janvi.11/12

--- HomeLoan/src/main/java/com/bank/model/DependentInfo.java
+++ HomeLoan/src/main/java/com/bank/model/DependentInfo.java
@@ -1,5 +1,21 @@
 package com.bank.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class DependentInfo {
+	
+	@Id
+	private int  dependentinfold;
+	private String  fatherName;
+	private String motherName;
+	private String spouseName;
+	private int  noOfFamilyMember;
+	private int  noOfChild;
+	private String maritalStatus;
+	private String dependentMember;
 
 }
