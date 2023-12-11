5e7a606075986615606be7c0db3955991ab29ac4
bhaiyya15 <bhaiyya.dandge15@gmail.com>
2023-12-11 11:11:33 +0530

    MainPro11/12/2023

---
+++ HomeLoan/src/main/java/com/bank/HomeLoan1Application.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoan1Application.class, args);
+	}
+
+}
