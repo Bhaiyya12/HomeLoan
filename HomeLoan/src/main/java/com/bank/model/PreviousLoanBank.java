87bf5c0903a978e582acad6f9e62826d9d90e7e0
Bhaiyya12 <153350920+Bhaiyya12@users.noreply.github.com>
2023-12-11 12:49:40 +0530

    Update PreviousLoanBank.java

--- HomeLoan/src/main/java/com/bank/model/PreviousLoanBank.java
+++ HomeLoan/src/main/java/com/bank/model/PreviousLoanBank.java
@@ -1,5 +1,14 @@
 package com.bank.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Data;
 
+
+@Data
+@Entity
 public class PreviousLoanBank {
 
 }
