42babfdd92ed9073b2b5cab5fab2ae576f460219
Bhaiyya12 <153350920+Bhaiyya12@users.noreply.github.com>
2023-12-11 12:06:17 +0530

    Update PermanentAddress.java

--- HomeLoan/src/main/java/com/bank/model/PermanentAddress.java
+++ HomeLoan/src/main/java/com/bank/model/PermanentAddress.java
@@ -1,5 +1,20 @@
 package com.bank.model;
+@Data
+@Entity
+public class PermanentAddress 
+{
+@Id	
+private int permanentAddressId ;
+private String areaname;
+private String cityname;
+private String district;
+private String state;
+private Long pincode;
+private int houseNumber;
+private String streetName;
 
-public class PermanentAddress {
 
+	
+
+}
 }
